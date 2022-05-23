545cbdd058af43cc7c2b31a233ad39e37e3decc2
HaonanDi <hdi0523@elutions.com>
2022-05-23 15:43:34 -0400

    first push

---
+++ test0.py
@@ -0,0 +1,13 @@
+print("hello world with elutions on vscode!")
+
+import numpy as np
+import pandas as pd
+
+a = np.array([1,3,4,5,2])
+print(a.mean())
+
+import statsmodels as sm
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+print('All packages are loaded correctly')
